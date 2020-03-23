292e1df0f25e2bb035fe07c15d8d5a4633959025
cobachie <7006340+cobachie@users.noreply.github.com>
2020-03-23 13:22:54 +0900

    display user info

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ function fetchUserInfo(userId) {
       } else {
         return response.json().then(userInfo => {
           console.log(userInfo);
+          const view = escapeHTML`
+            <h4>${userInfo.name} (@${userInfo.login})</h4>
+            <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+            <dl>
+              <dt>Location</dt>
+              <dd>${userInfo.location}</dd>
+              <dt>Repositories</dt>
+              <dd>${userInfo.public_repos}</dd>
+            </dl>
+            `;
+          const result = document.getElementById('result');
+          result.innerHTML = view;
         });
       }
     }).catch(error => {
@@ -14,3 +26,23 @@ function fetchUserInfo(userId) {
     })
 }
 
+function escapeSpecialChars(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+// タグ関数
+function escapeHTML(strings, ...values) {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === "string") {
+      return result + escapeSpecialChars(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
+}
