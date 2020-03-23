0026a0d3656c113f1b166d5837b9d9b1893fbea4
cobachie <7006340+cobachie@users.noreply.github.com>
2020-03-23 14:02:42 +0900

    refactor function

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
+// エントリーポイント
+function main() {
+  fetchUserInfo('cobachie')
+    .then((userInfo) => createView(userInfo))
+    .then((view) => displayView(view))
+    .catch((error) => {
+      console.error("エラーが発生しました（${error}）");
+    });
+}
+
 function fetchUserInfo(userId) {
   fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
     .then(response => {
       console.log(response.status);
       if (!response.ok) {
-        console.error("エラーレスポンス", response);
+        return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
       } else {
-        return response.json().then(userInfo => {
-          console.log(userInfo);
-          const view = escapeHTML`
-            <h4>${userInfo.name} (@${userInfo.login})</h4>
-            <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-            <dl>
-              <dt>Location</dt>
-              <dd>${userInfo.location}</dd>
-              <dt>Repositories</dt>
-              <dd>${userInfo.public_repos}</dd>
-            </dl>
-            `;
-          const result = document.getElementById('result');
-          result.innerHTML = view;
-        });
+        return response.json();
       }
-    }).catch(error => {
-      console.error(error);
-    })
+    });
+}
+
+function createView(userInfo) {
+  console.log(userInfo);
+  return escapeHTML`
+          <h4>${userInfo.name} (@${userInfo.login})</h4>
+          <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+          <dl>
+            <dt>Location</dt>
+            <dd>${userInfo.location}</dd>
+            <dt>Repositories</dt>
+            <dd>${userInfo.public_repos}</dd>
+          </dl>
+          `;
+}
+
+function displayView(view) {
+  const result = document.getElementById('result');
+  result.innerHTML = view;
 }
 
 function escapeSpecialChars(str) {
